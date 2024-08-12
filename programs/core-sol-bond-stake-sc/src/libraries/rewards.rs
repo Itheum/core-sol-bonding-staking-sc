285ee63cc4a855d15dcb4c92378aa3c2bf269a96
Bucur David <dbucur22@gmail.com>
2024-08-12 16:50:18 +0300

    fix: remove rewards cache

--- programs/core-sol-bond-stake-sc/src/libraries/rewards.rs
+++ programs/core-sol-bond-stake-sc/src/libraries/rewards.rs
@@ -1,20 +1,22 @@
-use crate::{get_current_slot, State, DIVISION_SAFETY_CONST, MAX_PERCENT, SLOTS_IN_YEAR};
+use crate::{
+    get_current_slot, RewardsConfig, State, DIVISION_SAFETY_CONST, MAX_PERCENT, SLOTS_IN_YEAR,
+};
 use anchor_lang::prelude::*;
 
-use super::{MulDiv, RewardsCache};
+use super::full_math::MulDiv;
 
-fn generate_aggregated_rewards(
+fn generate_aggregated_rewards<'info>(
     total_bond_amount: u64,
-    rewards_cache: &mut RewardsCache,
+    rewards_config: &mut Account<'info, RewardsConfig>,
 ) -> Result<()> {
-    let last_reward_slot = rewards_cache.last_reward_slot;
-    let extra_rewards_unbounded = calculate_rewards_since_last_allocation(rewards_cache)?;
-    let max_apr = rewards_cache.max_apr;
+    let last_reward_slot = rewards_config.last_reward_slot;
+    let extra_rewards_unbounded = calculate_rewards_since_last_allocation(rewards_config)?;
+    let max_apr = rewards_config.max_apr;
 
     let extra_rewards: u64;
     if max_apr == 0 {
         let extra_rewards_apr_bonded_per_slot =
-            get_amount_apr_bounded(rewards_cache.max_apr, rewards_cache.rewards_reserve);
+            get_amount_apr_bounded(rewards_config.max_apr, rewards_config.rewards_reserve);
 
         let current_slot = get_current_slot()?;
 
@@ -27,14 +29,14 @@ fn generate_aggregated_rewards(
         extra_rewards = extra_rewards_unbounded;
     }
 
-    if extra_rewards > 0 && extra_rewards <= rewards_cache.rewards_reserve {
+    if extra_rewards > 0 && extra_rewards <= rewards_config.rewards_reserve {
         let increment = extra_rewards
             .mul_div_floor(DIVISION_SAFETY_CONST, total_bond_amount)
             .unwrap();
 
-        rewards_cache.rewards_per_share += increment;
-        rewards_cache.rewards_reserve -= extra_rewards;
-        rewards_cache.accumulated_rewards += extra_rewards;
+        rewards_config.rewards_per_share += increment;
+        rewards_config.rewards_reserve -= extra_rewards;
+        rewards_config.accumulated_rewards += extra_rewards;
     }
 
     Ok(())
@@ -44,26 +46,28 @@ fn get_amount_apr_bounded(max_apr: u64, amount: u64) -> u64 {
     amount * max_apr / MAX_PERCENT / SLOTS_IN_YEAR
 }
 
-fn calculate_rewards_since_last_allocation(rewards_cache: &mut RewardsCache) -> Result<u64> {
+fn calculate_rewards_since_last_allocation<'info>(
+    rewards_config: &mut Account<'info, RewardsConfig>,
+) -> Result<u64> {
     let current_slot = get_current_slot()?;
 
-    if !rewards_cache.rewards_state == State::Active.to_code() {
+    if !rewards_config.rewards_state == State::Active.to_code() {
         return Ok(0u64);
     }
 
-    if current_slot <= rewards_cache.last_reward_slot {
+    if current_slot <= rewards_config.last_reward_slot {
         return Ok(0u64);
     }
 
-    let slot_diff = current_slot - rewards_cache.last_reward_slot;
+    let slot_diff = current_slot - rewards_config.last_reward_slot;
 
-    rewards_cache.last_reward_slot = current_slot;
+    rewards_config.last_reward_slot = current_slot;
 
-    Ok(rewards_cache.rewards_per_slot * slot_diff)
+    Ok(rewards_config.rewards_per_slot * slot_diff)
 }
 
-fn calculate_address_share_in_rewards(
-    rewards_cache: &RewardsCache,
+fn calculate_address_share_in_rewards<'info>(
+    rewards_config: &mut Account<'info, RewardsConfig>,
     address_bond_amount: u64,
     address_last_reward_slot: u64,
     total_bond_amount: u64,
@@ -73,13 +77,13 @@ fn calculate_address_share_in_rewards(
         return 0;
     }
 
-    if rewards_cache.accumulated_rewards == 0 {
+    if rewards_config.accumulated_rewards == 0 {
         return 0;
     }
 
     let address_rewards = address_bond_amount
         .mul_div_floor(
-            (rewards_cache.rewards_per_share - address_last_reward_slot),
+            rewards_config.rewards_per_share - address_last_reward_slot,
             DIVISION_SAFETY_CONST,
         )
         .unwrap();
